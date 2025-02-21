a3420b683ba3410818172d745c0ddda2d8076de5
shivamani39 <150172113+shivamani39@users.noreply.github.com>
2025-02-22 02:06:25 +0530

    using nested_routes

--- index.jsx
+++ index.jsx
@@ -5,22 +5,27 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import Layout from './components/Layout';
+import HostLayout from './components/HostLayout';
 import "./server"
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />}/>
-        <Route path="/vans/:id" element={<VanDetail/>}/>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />}/>
+          <Route path="/vans/:id" element={<VanDetail/>}/>
+          <Route path="host" element={<HostLayout/>}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/Host/Reviews.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard(){
+    return(
+        <>
+            <h1>Review is here</h1>
+        </>
+    )
+}
