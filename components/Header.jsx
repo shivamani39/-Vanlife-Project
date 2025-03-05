10c1282b95fd428ecf77f687e2994209923cdbd5
shivamani39 <150172113+shivamani39@users.noreply.github.com>
2025-03-05 18:56:53 +0530

    authentication and sending user to original page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,29 +1,46 @@
 import React from "react"
-import { Link,NavLink } from "react-router-dom"
-export default function Header(){
+import { Link, NavLink } from "react-router-dom"
+
+export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-            <NavLink 
+                <NavLink
                     to="/host"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/about"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
-                <NavLink 
+                <NavLink
                     to="/vans"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
-
+                <Link to="login" className="login-link">
+                    <img
+                        src="../assets/images/avatar-icon.png"
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
