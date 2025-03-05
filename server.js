5bb3f7b8ef38b4cbb20e4e4060b3f5b1c466d067
shivamani39 <150172113+shivamani39@users.noreply.github.com>
2025-03-05 19:24:11 +0530

    completing host pages

--- server.js
+++ server.js
@@ -34,23 +34,27 @@ createServer({
         })
 
         this.get("/host/vans", (schema, request) => {
-            
+            // Hard-code the hostId for now
             return schema.vans.where({ hostId: "123" })
         })
 
         this.get("/host/vans/:id", (schema, request) => {
-          
+            // Hard-code the hostId for now
             const id = request.params.id
             return schema.vans.findBy({ id, hostId: "123" })
         })
         
         this.post("/login", (schema, request) => {
             const { email, password } = JSON.parse(request.requestBody)
-            
+            // ⚠️ This is an extremely naive version of authentication. Please don't
+            // do this in the real world, and never save raw text passwords
+            // in your database 😅
             const foundUser = schema.users.findBy({ email, password })
             if (!foundUser) {
                 return new Response(401, {}, { message: "No user with those credentials found!" })
             }
+
+            // At the very least, don't send the password back to the client 😅
             foundUser.password = undefined
             return {
                 user: foundUser,
